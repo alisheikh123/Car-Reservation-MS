8c1be66ae6e122890744f990526a75a67dbc7d68
Ali Raza <fitnessh6@outlook.com>
2020-12-01 23:59:36 +0500

    ffg

--- AdminLTE.MVC/wwwroot/js/site.js
+++ AdminLTE.MVC/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+angular.module("myApp", []).run(function ($routeScope) {
+    $routeScope.color = "Blue"
+
+}).controller("myCt", function ($scope) {
+    $scope.FirstName = "Ali",
+        $scope.LastName = "Tahir",
+        $scope.Name = "Happy Birthday",
+        $scope.color = "Green",
+        $scope.changename = function () {
+        $scope.Name = "Happy Birthday Ali! :-)"
+        };
+        
+        $scope.Sum = function () {
+            return $scope.FirstName + " " + $scope.LastName;
+
+        };
+
+    });
+    
